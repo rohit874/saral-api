1e36e0938045c3fdd70079a0fecd0bc5344c0ec4
Rohit Kumar <rohitk9908@gmail.com.com>
2022-02-08 18:59:58 +0530

    port changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 var cors = require('cors')
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
